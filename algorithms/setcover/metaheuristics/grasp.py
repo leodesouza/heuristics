dffeaf5e53c6e922625e0b5366244e28a732e40c
leonardosouza <leo.desouza@gmail.com>
2023-12-23 19:06:58 -0300

    little refactoring

--- algorithms/setcover/metaheuristics/grasp.py
+++ algorithms/setcover/metaheuristics/grasp.py
@@ -1,42 +1,45 @@
+import random
+
 from algorithms.setcover.local_search import create_local_search_solution, generate_neighborhood, generate_neighborhood_2
+from algorithms.utils.pseudo_random_generator import create_index
 from solution_evaluation import evaluate
 
 
-def create_local_search_solution_grasp(subsets, current_solution):
-    # ([([0, 1, 2], '3'), ([3, 4], '5'), ([3, 4], '5')], 13)
-    improved = True
-    current_evaluation = evaluate(current_solution)
-
-    while improved:
-        improved = False
-        best_neighbor = None
-        best_neighbor_evaluation = current_evaluation
+def create_local_search_solution_grasp(pseudo_random, current_solution):
+    improved = False
+    percentage = 10
+    while not improved:
+        # best_neighbor = None
+        best_neighbor_evaluation = evaluate(current_solution)
         neighborhood = generate_neighborhood_2(current_solution)
+        max_solutions = int((percentage / 100) * len(neighborhood))
+        neighborhood = sorted(neighborhood, key=lambda x: x[0])
+        neighborhood = neighborhood[:max_solutions]
         space_count = len(neighborhood)
-
-        for neighbor in neighborhood:
-            space_count -= 1
+        while space_count != 0:
+            random_index = create_index(0, len(neighborhood) - 1, pseudo_random)
+            neighbor = neighborhood[random_index]
             neighbor_evaluation = evaluate(neighbor)
             if neighbor_evaluation < best_neighbor_evaluation:
                 best_neighbor = neighbor
                 best_neighbor_evaluation = evaluate(best_neighbor)
+                current_evaluation = best_neighbor_evaluation
+                current_solution = neighbor
+                improved = True
+            space_count -= 1
 
-        if best_neighbor_evaluation < current_evaluation:
-            current_evaluation = best_neighbor_evaluation
-            improved = True
-
-        if improved or space_count == 0:
+        if space_count == 0:
             break
 
     return current_solution
 
 
-def create_grasp_solution(subsets, inicial_solution, max_iterations=100):
+def create_grasp_solution(subsets, inicial_solution, pseudo_random, max_iterations=100):
     best_solution = None
     best_evaluation = float('inf')
     for _ in range(max_iterations):
         # candidate_solution = create_local_search_solution(subsets, inicial_solution, max_iterations)
-        candidate_solution = create_local_search_solution_grasp(subsets, inicial_solution)
+        candidate_solution = create_local_search_solution_grasp(pseudo_random, inicial_solution)
         candidate_solution = candidate_solution
         candidate_evaluation = evaluate(candidate_solution)
 
