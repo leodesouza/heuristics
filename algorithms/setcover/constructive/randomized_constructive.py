7758f8102e148901ea5a5a9cd472abf123788c7e
leonardosouza <leo.desouza@gmail.com>
2023-12-30 10:26:45 -0300

    randomized_constructive fix to loop through solutions until coverage of all elements

--- algorithms/setcover/constructive/randomized_constructive.py
+++ algorithms/setcover/constructive/randomized_constructive.py
@@ -1,10 +1,13 @@
 from algorithms.utils.pseudo_random_generator import generate_rand_permutation
+from redundancy_elimination import remove_rendundancy
 
 
 def create_randomized_constructive(subsets_and_costs, pseudo_random):
     elements = set()
+    aux_elements = set()
     for subSet in subsets_and_costs:
         elements.update(subSet[0])
+        aux_elements.update(subSet[0])
     elements = list(elements)
 
     shuffled_vector = iter(generate_rand_permutation(len(elements), pseudo_random))
@@ -12,20 +15,24 @@ def create_randomized_constructive(subsets_and_costs, pseudo_random):
     solution = []
     number_elements = len(elements)
 
-    while number_of_covered < number_elements:
+    while len(aux_elements) > 0:
         random_index = next(shuffled_vector)
         current = elements[random_index]
-        subsets_that_cover_current = [subs for subs in subsets_and_costs if current in subs[0]]
+        aux_elements.remove(current)
+        subsets_that_cover_current = [subs for subs in subsets_and_costs if current in subs[0] and not any(e in solution for e in subs)]
         rand_permutation_of_subsets = generate_rand_permutation(len(subsets_that_cover_current), pseudo_random)
         index_of_first_subset = rand_permutation_of_subsets[0]
         # local_solution variable is not relate to local search algorithm
         local_solution = subsets_that_cover_current[index_of_first_subset]
-        number_of_covered += 1
-        for e in local_solution:
-            if e != current:
-                found = any(e in sublist for sublist in solution)
-                if not found:
-                    number_of_covered += 1
+        # number_of_covered += 1
+        # for e in local_solution:
+        #     if e != current:
+        #         found = any(e in sublist for sublist in solution)
+        #         if not found:
+        #             number_of_covered += 1
         solution.append(local_solution)
+
+    # solution = remove_rendundancy((solution, 0))
+    # return solution
 
     return solution, sum(int(sub[1]) for sub in solution)
