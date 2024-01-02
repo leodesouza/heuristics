5d7d71ffeeb72a4964fbc8e9f78d8faf5304fdb4
leonardosouza <leo.desouza@gmail.com>
2024-01-01 22:38:33 -0300

    adding logging

--- algorithms/scp_v2.py
+++ algorithms/scp_v2.py
@@ -6,6 +6,15 @@ from algorithms.utils.pseudo_random_generator import lecuyer_rando
 from algorithms.setcover.metaheuristics.grasp import create_grasp_solution
 from algorithms.setcover.metaheuristics.iterated_local_search import create_iterated_local_search
 from algorithms.setcover.metaheuristics.simulated_annealing import create_simulated_annealing
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+log_file = 'logfile.log'
+file_handler = logging.FileHandler(log_file)
+file_formatter = '%(asctime)s - %(levelname)s - %(message)s'
+file_handler.setFormatter(file_formatter)
+logger = logging.getLogger('')
+logger.addHandler(file_handler)
 
 
 def write_to_file(instance_name, m, n, best, hc1_weight, hc1_re, mh1_weight,
@@ -19,99 +28,104 @@ def write_to_file(instance_name, m, n, best, hc1_weight, hc1_re, mh1_weight,
 class ScpV2:
 
     def process_instances(self, parameters, _max_experiments_iteration):
+        logging.info("start processing files")
+        try:
+            files = os.listdir(parameters.source)
+            already_wrote_head = False
+            for file_name in files:
+                if file_name == parameters.output_file:
+                    continue
 
-        files = os.listdir(parameters.source)
-        already_wrote_head = False
-        for file_name in files:
-            if file_name == parameters.output_file:
-                continue
+                m = 0  # elements
+                n = 0  # subsets
+                file_path = os.path.join(parameters.source, file_name)
+                m, n, subsets = self.get_subsets(file_path)
+                pseudo_random = lecuyer_rando(parameters.seed)
 
-            m = 0  # elements
-            n = 0  # subsets
-            file_path = os.path.join(parameters.source, file_name)
-            m, n, subsets = self.get_subsets(file_path)
-            pseudo_random = lecuyer_rando(parameters.seed)
+                # ** Start CPU Time **
+                start_cpu_time = time.process_time()
+                hc1_re = 0
+                hc2_re = 0
+                hc3_re = 0
 
-            # ** Start CPU Time **
-            start_cpu_time = time.process_time()
-            hc1_re = 0
-            hc2_re = 0
-            hc3_re = 0
+                if 'hc1' in parameters.options:
+                    constructive_result_1 = randomized_constructive.create_randomized_constructive(subsets,
+                                                                                                   pseudo_random)
+                    hc1_weight = constructive_result_1[1]
+                if 'hc2' in parameters.options:
+                    constructive_result_2 = greedy_constructive.create_greedy_constructive(subsets)
+                    hc2_weight = constructive_result_2[1]
 
-            if 'hc1' in parameters.options:
-                constructive_result_1 = randomized_constructive.create_randomized_constructive(subsets, pseudo_random)
-                hc1_weight = constructive_result_1[1]
-            if 'hc2' in parameters.options:
-                constructive_result_2 = greedy_constructive.create_greedy_constructive(subsets)
-                hc2_weight = constructive_result_2[1]
+                if 'hc3' in parameters.options:
+                    constructive_result_3 = weighted_greedy_constructive.create_wighted_greedy_constructive(subsets)
+                    hc3_weight = constructive_result_3[1]
 
-            if 'hc3' in parameters.options:
-                constructive_result_3 = weighted_greedy_constructive.create_wighted_greedy_constructive(subsets)
-                hc3_weight = constructive_result_3[1]
+                if 're' in parameters.options:
+                    constructive_result_1 = redundancy_elimination.remove_rendundancy(constructive_result_1)
+                    hc1_re = constructive_result_1[1]
+                    constructive_result_2 = redundancy_elimination.remove_rendundancy(constructive_result_2)
+                    hc2_re = constructive_result_2[1]
+                    constructive_result_3 = redundancy_elimination.remove_rendundancy(constructive_result_3)
+                    hc3_re = constructive_result_3[1]
 
-            if 're' in parameters.options:
-                constructive_result_1 = redundancy_elimination.remove_rendundancy(constructive_result_1)
-                hc1_re = constructive_result_1[1]
-                constructive_result_2 = redundancy_elimination.remove_rendundancy(constructive_result_2)
-                hc2_re = constructive_result_2[1]
-                constructive_result_3 = redundancy_elimination.remove_rendundancy(constructive_result_3)
-                hc3_re = constructive_result_3[1]
+                metaheuristic_method_used = ''
+                if 'grasp' in parameters.options:
+                    metaheuristic_method_used = 'GRASP'
+                    # metaheuristic_results = create_grasp_solution(subsets, constructive_result[0], 5)
+                    # metaheuristic_total_weight = metaheuristic_results[1]
 
-            metaheuristic_method_used = ''
-            if 'grasp' in parameters.options:
-                metaheuristic_method_used = 'GRASP'
-                # metaheuristic_results = create_grasp_solution(subsets, constructive_result[0], 5)
-                # metaheuristic_total_weight = metaheuristic_results[1]
+                    result1 = create_grasp_solution(subsets, constructive_result_1[0], pseudo_random,
+                                                    _max_experiments_iteration)
+                    result2 = create_grasp_solution(subsets, constructive_result_2[0], pseudo_random,
+                                                    _max_experiments_iteration)
+                    result3 = create_grasp_solution(subsets, constructive_result_3[0], pseudo_random,
+                                                    _max_experiments_iteration)
+                    mh1_weight = result1[1]
+                    mh2_weight = result2[1]
+                    mh3_weight = result3[1]
+                elif 'itlocalsearch' in parameters.options:
+                    metaheuristic_method_used = 'ILS'
+                    result1 = create_iterated_local_search(constructive_result_1[0], _max_experiments_iteration)
+                    result2 = create_iterated_local_search(constructive_result_2[0], _max_experiments_iteration)
+                    result3 = create_iterated_local_search(constructive_result_3[0], _max_experiments_iteration)
+                    mh1_weight = result1[1]
+                    mh2_weight = result2[1]
+                    mh3_weight = result3[1]
 
-                result1 = create_grasp_solution(subsets, constructive_result_1[0], pseudo_random,
-                                                _max_experiments_iteration)
-                result2 = create_grasp_solution(subsets, constructive_result_2[0], pseudo_random,
-                                                _max_experiments_iteration)
-                result3 = create_grasp_solution(subsets, constructive_result_3[0], pseudo_random,
-                                                _max_experiments_iteration)
-                mh1_weight = result1[1]
-                mh2_weight = result2[1]
-                mh3_weight = result3[1]
-            elif 'itlocalsearch' in parameters.options:
-                metaheuristic_method_used = 'ILS'
-                result1 = create_iterated_local_search(constructive_result_1[0], _max_experiments_iteration)
-                result2 = create_iterated_local_search(constructive_result_2[0], _max_experiments_iteration)
-                result3 = create_iterated_local_search(constructive_result_3[0], _max_experiments_iteration)
-                mh1_weight = result1[1]
-                mh2_weight = result2[1]
-                mh3_weight = result3[1]
+                elif 'sa' in parameters.options:
+                    metaheuristic_method_used = 'SA'
+                    result1 = create_simulated_annealing(subsets, constructive_result_1[0], _max_experiments_iteration)
+                    result2 = create_simulated_annealing(subsets, constructive_result_2[0], _max_experiments_iteration)
+                    result3 = create_simulated_annealing(subsets, constructive_result_3[0], _max_experiments_iteration)
+                    mh1_weight = result1[1]
+                    mh2_weight = result2[1]
+                    mh3_weight = result3[1]
 
-            elif 'sa' in parameters.options:
-                metaheuristic_method_used = 'SA'
-                result1 = create_simulated_annealing(subsets, constructive_result_1[0], _max_experiments_iteration)
-                result2 = create_simulated_annealing(subsets, constructive_result_2[0], _max_experiments_iteration)
-                result3 = create_simulated_annealing(subsets, constructive_result_3[0], _max_experiments_iteration)
-                mh1_weight = result1[1]
-                mh2_weight = result2[1]
-                mh3_weight = result3[1]
+                end_cpu_time = time.process_time()
+                cpu_executation_time = round((end_cpu_time - start_cpu_time) / 1.0, 1) * 1000
 
-            end_cpu_time = time.process_time()
-            cpu_executation_time = round((end_cpu_time - start_cpu_time) / 1.0, 1) * 1000
+                # ** End CPU Time **
 
-            # ** End CPU Time **
+                output_file = os.path.join(parameters.source, parameters.output_file)
+                file_name, extension = os.path.splitext(file_name)
+                if not already_wrote_head:
+                    write_to_file('Inst.', 'm', 'n', 'Best', 'HC1', '+ER1',
+                                  f'{metaheuristic_method_used}1',
+                                  'HC2', '+ER2',
+                                  f'{metaheuristic_method_used}2',
+                                  'HC3', '+ER3',
+                                  f'{metaheuristic_method_used}3',
+                                  'TEMPO(ms)', output_file)
+                    already_wrote_head = True
 
-            output_file = os.path.join(parameters.source, parameters.output_file)
-            file_name, extension = os.path.splitext(file_name)
-            if not already_wrote_head:
-                write_to_file('Inst.', 'm', 'n', 'Best', 'HC1', '+ER1',
-                              f'{metaheuristic_method_used}1',
-                              'HC2', '+ER2',
-                              f'{metaheuristic_method_used}2',
-                              'HC3', '+ER3',
-                              f'{metaheuristic_method_used}3',
-                              'TEMPO(ms)', output_file)
-                already_wrote_head = True
-
-            best_value = 0
-            write_to_file(file_name, m, n, best_value, hc1_weight, hc1_re, mh1_weight,
-                          hc2_weight, hc2_re, mh2_weight,
-                          hc3_weight, hc3_re, mh3_weight,
-                          cpu_executation_time, output_file)
+                best_value = 0
+                write_to_file(file_name, m, n, best_value, hc1_weight, hc1_re, mh1_weight,
+                              hc2_weight, hc2_re, mh2_weight,
+                              hc3_weight, hc3_re, mh3_weight,
+                              cpu_executation_time, output_file)
+        except BaseException as e:
+            # logging.error("An error occurred: %s", e, exc_info=True)  # exc_info=True logs the traceback
+            logging.error("Error: %s", e, exc_info=True)
 
     @staticmethod
     def get_subsets(scp_file):
